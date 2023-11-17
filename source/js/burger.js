e520622325a9c3a04429ef5003ddc6177fbca7ef
Sergey Ardintsev <archunter13@yandex.ru>
2023-11-17 17:42:07 +0300

    svg adaptive

--- source/js/burger.js
+++ source/js/burger.js
@@ -1,10 +1,10 @@
 const openBtn = document.querySelector('.main-nav__burger');
-const nav = document.querySelector('..main-nav__list');
-const body = document.querySelector('body')
+      const nav = document.querySelector('.main-nav__list');
+      // const body = document.querySelector('body')
 
-openBtn.addEventListener('click', e=> {
-    nav.classList.toggle('.main-nav__list--visible')
-    openBtn.classList.toggle('main-nav__burger--opened')
-    body.classList.toggle('body--locked')
-  }
-)
+      openBtn.addEventListener('click', e=> {
+          nav.classList.toggle('main-nav__list--visible')
+          openBtn.classList.toggle('main-nav__burger--opened')
+          // body.classList.toggle('body--locked')
+        }
+      )
